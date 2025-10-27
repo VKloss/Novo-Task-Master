e03eb934cb0095ccf2b3eb171a646fd9ab9a489f
VKloss <vitorkloss9000@gmail.com>
2025-10-27 18:20:51 -0300

    update

--- Taskmaster/cadastro.js
+++ Taskmaster/cadastro.js
@@ -1,23 +1,33 @@
 function cadastrar(event) {
-    event.preventDefault();
-    const firstname = document.getElementById('firstName').value.trim();
-    const lastname = document.getElementById('lastName').value;
-    const email = document.getElementById('email').value;
-    const password = document.getElementById('password').value;
+  event.preventDefault();
 
-    const novoRegistro = {
+  const firstname = document.getElementById('firstName').value.trim();
+  const lastname = document.getElementById('lastName').value.trim();
+  const email = document.getElementById('email').value.trim();
+  const password = document.getElementById('password').value.trim();
+
+  auth.createUserWithEmailAndPassword(email, password)
+    .then(userCredential => {
+      const user = userCredential.user;
+   
+      return db.ref('usuarios/' + user.uid).set({
         firstname,
         lastname,
-        email,
-        password
-    };
-
-  db.ref('uservitor').push(novoRegistro)
+        email
+      });
+    })
     .then(() => {
-      alert("Registrado com sucesso! ");
-        document.getElementById('firstName').value = '';
-        document.getElementById('lastName').value = '';
-        document.getElementById('email').value = '';
-        document.getElementById('password').value = '';
+      alert("Conta criada com sucesso! 🎉");
+      window.location.href = "login.html";
+    })
+    .catch(error => {
+      alert("Erro ao cadastrar: " + error.message);
     });
 }
+
+document.getElementById('btnVoltar').addEventListener('click', function() {
+  window.location.href = 'login.html';
+});
+document.getElementById('signup-btn').addEventListener('click', function() {
+    window.location.href = 'login.html';
+});
--- Taskmaster/firebase.js
+++ Taskmaster/firebase.js
@@ -1,14 +1,15 @@
 const firebaseConfig = {
-
-    apiKey: "AIzaSyDcn0AfM0ljA72-c7v8u86FCYA3uPMnBxw",
-    authDomain: "arquivox-c9d74.firebaseapp.com",
-    databaseURL: "https://arquivox-c9d74-default-rtdb.firebaseio.com",
-    projectId: "arquivox-c9d74",
-    storageBucket: "arquivox-c9d74.firebasestorage.app",
-    messagingSenderId: "235350186717",
-    appId: "1:235350186717:web:6273c8f7e998257d5e9934"
+    apiKey: "AIzaSyDSb6em9Pk8Z-xjPyRmzpcU_DYdFd12P4k",
+    authDomain: "taskmaster-19436.firebaseapp.com",
+    databaseURL: "https://taskmaster-19436-default-rtdb.firebaseio.com",
+    projectId: "taskmaster-19436",
+    storageBucket: "taskmaster-19436.firebasestorage.app",
+    messagingSenderId: "26527596417",
+    appId: "1:26527596417:web:0971755a9bfa9772357280"
 
 };
 
 firebase.initializeApp(firebaseConfig);
+
+const auth = firebase.auth();
 const db = firebase.database();
